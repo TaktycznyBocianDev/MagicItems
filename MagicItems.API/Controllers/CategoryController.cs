c26c174cf94c2936a0dce14595310aa71c227c34
Stanisław Franczak <112652279+TaktycznyBocianDev@users.noreply.github.com>
2024-05-10 00:28:59 +0200

    CRD (no U) for Categories in API

--- MagicItems.API/Controllers/CategoryController.cs
+++ MagicItems.API/Controllers/CategoryController.cs
@@ -1,6 +1,11 @@
 using MagicItems.Shared.Models;
 using MagicItems.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Collections.Generic;
+using MagicItems.Shared.DTOs;
+using static MudBlazor.CategoryTypes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MagicItems.Api.Controllers;
 
@@ -16,16 +21,46 @@ public class CategoryController : ControllerBase
         _dapper = new DataContextDapper(configuration);
     }
 
-    [HttpGet("GetCategory/{Id}")]
-    public IEnumerable<Categories> GetCategory(int Id = 0)
+    [HttpGet("GetCategory/{Id}/{CategoryName}")]
+    public IEnumerable<Categories> GetCategory(int Id = 0, string? CategoryName = "none")
     {
 
         string sql = "EXEC GetCategories";
+        string parameters = "";
     
 
-        if (Id != 0) sql += " @Id = @Id";
+        if (Id != 0) parameters += ", @Id = @Id";
+        if (CategoryName?.ToLower() != "none") parameters += ", @CategoryName = @CategoryName";
 
+        if (parameters.StartsWith(",")) sql += parameters.Substring(1);
 
-        return _dapper.LoadData<Categories>(sql, new { Id = Id });
-    } 
+        return _dapper.LoadData<Categories>(sql, new { Id = Id, CategoryName = CategoryName });
+    }
+
+    [HttpPut("CreateCategory/{CategoriesDTO}")]
+    public IActionResult CreateCategory(CategoriesDTO newCategory)
+    {
+        string sql = "EXEC CreateCustomCategory @newCategory";
+
+        _dapper.ExecuteSql(sql, new {@newCategory = newCategory.CategoryName });
+
+        return Ok();
+    }
+
+    [HttpDelete("DeleteCategory/{Id}/{CategoryName}")]
+    public IActionResult DeleteCategory(int Id = 0, string? CategoryName = "none")
+    {
+        string sql = "EXEC RemoveCategory";
+        string parameters = "";
+
+        if (Id != 0) parameters += ", @Id = @Id";
+        if (CategoryName?.ToLower() != "none") parameters += ", @CategoryName = @CategoryName";
+
+        if (parameters.StartsWith(",")) sql += parameters.Substring(1);
+
+        _dapper.ExecuteSql(sql, new { Id = Id, CategoryName = CategoryName });
+
+        return Ok();
+    }
+
 }           
---
+++ MagicItems.Shared/DTOs/CategoriesDTO.cs
@@ -0,0 +1,7 @@
+namespace MagicItems.Shared.DTOs
+{
+    public partial class CategoriesDTO
+    {
+        public string CategoryName { get; set; } = "Custom Category";
+    }
+}
